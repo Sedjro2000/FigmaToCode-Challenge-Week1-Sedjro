540c4ff0b397e2662c8003e95f675ecc9a9538c9
Sedjro2000 <manwenselove@gmail.com>
2024-01-23 12:04:27 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import React from 'react'
+import Footer from './components/footer'
+
+function App() {
+
+
+  return (
+    <>
+      <div>Bonjur</div>
+      <Footer />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+     <div className='bg-blue-500 text-white p-4 w-full h-32'>
+        Footer
+
+     </div>
+
+    </>
+   
+  )
+}
+
+export default Footer
